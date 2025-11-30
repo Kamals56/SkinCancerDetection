3d621be3543f7e121a23dcd9a894f81083a04a0e
kamallamichhanes56 <kamallamichhane222@gmail.com>
2025-11-30 11:42:33 +0200

    added scheduler

--- args.py
+++ args.py
@@ -10,13 +10,13 @@ def get_args():
                         
     parser.add_argument("-csv_dir", type=str, default=os.path.join(BASE_DIR, "data/HAM10000_5folds.csv"))
                         
-    parser.add_argument("-out_dir", type=str, default=os.path.join(BASE_DIR, "outputs"))
+    parser.add_argument("-out_dir", type=str, default=os.path.join(BASE_DIR, "session"))
 
     parser.add_argument("-n_splits", type=int, default=5, help="Number of folds for cross-validation")
                         
     # Training parameters
 
-    parser.add_argument("-epochs", type=int, default=10, help="Number of training epochs")
+    parser.add_argument("-epochs", type=int, default=50, help="Number of training epochs")
 
     parser.add_argument("-batch_size", type=int, default=32,
                         choices = [16, 32, 64])
@@ -29,9 +29,23 @@ def get_args():
     
     parser.add_argument("-seed", type=int, default=42, help="Random seed for reproducibility")
 
+    # Model
     parser.add_argument("-backbone", type=str, default="resnet50",
                         choices=["resnet18", "resnet34", "resnet50"])
     parser.add_argument("-pretrained", type=bool, default=True, help="Use pretrained weights or not")
+
+    #Early stopping
+    parser.add_argument("-patience", type=int, default=7, help="Early stopping patience")
+
+    # LR scheduler
+
+    parser.add_argument("-scheduler", type=str, default="plateau", choices=["plateau", "steplr", "cosine"],
+                        help="Type of LR scheduler to use")
+    parser.add_argument("-scheduler_patience", type=int, default=3, help="Patience for LR scheduler if using plateau")
+
+    parser.add_argument("-scheduler_factor", type=float, default=0.1, help="Factor to reduce LR")
+
+    parser.add_argument("-scheduler_step_size", type=int, default=10, help="Step size for StepLR")
 
     args = parser.parse_args()
     return args
